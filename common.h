9ba7b004fb59fd611a6805867b3ec593f77e130e
Kevin Zhen <kz72@cornell.edu>
2024-04-25 22:13:42 -0700

    updated cell size

--- common.h
+++ common.h
@@ -2,7 +2,7 @@
 #define NUMCOLS 100
 #define pi 3.14159265358979323
 
-#define delx 0.01         // Cell Size
+#define delx 0.0015       // Cell Size
 #define delt (delx / 6e8) // Time Step
 #define eps0 8.8e-12;
 #define mu0 (4 * pi * 1e-7)
