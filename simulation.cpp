f9cc8294f1bf4dce4ba695752d516fcbf4f767a5
Kevin Zhen <kz72@cornell.edu>
2024-05-16 12:26:17 -0700

    updated code

--- simulation.cpp
+++ simulation.cpp
@@ -14,7 +14,6 @@ using namespace std;
 
 #include "common.h"
 
-
 #define OMP_THREADS 64
 #define MIN_DEN 0.001
 #define grad_w 0.0001
@@ -44,12 +43,12 @@ int *source_x;
 int *source_y;
 double *source_amp;
 double *source_offset;
-int * source_type;
+int *source_type;
 
-int * mat_x;
-int * mat_y;
-double * mat_eps;
-double * mat_sig;
+int *mat_x;
+int *mat_y;
+double *mat_eps;
+double *mat_sig;
 
 double d_eps;
 double d_sig;
@@ -65,12 +64,14 @@ double *relative_eps, *sigma;
 double damage_max;
 int fdtd_steps, topop_steps, NUMROWS, NUMCOLS;
 
-double mat_cost(){
+double mat_cost()
+{
 	double cost = 0.0;
-	for(int i = 0; i < n_design; ++i){
+	for (int i = 0; i < n_design; ++i)
+	{
 		cost += design_density[i];
 	}
-	cost/=n_design;	
+	cost /= n_design;
 	return cost * 0.1;
 }
 void init_mat_properties()
@@ -85,35 +86,32 @@ void init_mat_properties()
 		}
 	}
 
-	//initialize materials other than design ps
+	// initialize materials other than design ps
 	for (int i = 0; i < n_mats; ++i)
 	{
 
 		relative_eps(mat_x[i], mat_y[i]) = mat_eps[i];
 		sigma(mat_x[i], mat_y[i]) = mat_sig[i];
 	}
-
 }
 
 void update_d_mat()
 {
-	//update material space with design params
+	// update material space with design params
 	for (int i = 0; i < n_design; ++i)
-	{	
+	{
 		relative_eps(design_x[i], design_y[i]) = design_density[i] * (d_eps - 1) + 1;
 		sigma(design_x[i], design_y[i]) = design_density[i] * d_sig;
 	}
-
 }
 
 void set_d_mat(int dof, double dens)
 {
-	//set a single design material square to some density
+	// set a single design material square to some density
 	design_density[dof] = dens;
 	relative_eps(design_x[dof], design_y[dof]) = dens * (d_eps - 1) + 1;
 	sigma(design_x[dof], design_y[dof]) = dens * d_sig;
 }
-
 
 double simulate()
 {
@@ -128,7 +126,7 @@ double simulate()
 
 		for (int i = 0; i < n_targets; ++i)
 		{
-			damage += pow(ex(target_x[i], target_y[i]),2)*delt*delx*delx;
+			damage += pow(ex(target_x[i], target_y[i]), 2) * delt * delx * delx;
 		}
 	}
 
@@ -138,33 +136,31 @@ double simulate()
 void update_density()
 {
 
-	
 	double sum = 0;
 	for (int d = 0; d < d_perr_id; ++d)
 	{
 		int d_id = r_id * d_perr_id + d;
-		if(d_id < n_design){
+		if (d_id < n_design)
+		{
 			double old_val = design_density[d_id];
 			// we need to estimate the gradient of the score - central difference with each design variable
 			set_d_mat(d_id, old_val - grad_w);
-			double back = simulate()/damage_max + mat_cost();
+			double back = simulate() / damage_max + mat_cost();
 			set_d_mat(d_id, old_val + grad_w);
-			double front = simulate()/damage_max + mat_cost();
+			double front = simulate() / damage_max + mat_cost();
 
 			lcl_grad[d] = (front - back) / (2 * grad_w);
 			sum += lcl_grad[d] * lcl_grad[d];
-			//set it back
+			// set it back
 			set_d_mat(d_id, old_val);
 		}
-
-
 	}
-	
-	//combine magnitudes and gradients
+
+	// combine magnitudes and gradients
 	double total_mag;
 	MPI_Allgather(lcl_grad, d_perr_id, MPI_DOUBLE, design_grad, d_perr_id, MPI_DOUBLE, MPI_COMM_WORLD);
 	MPI_Barrier(MPI_COMM_WORLD);
-cout << "rank " << r_id << " updating density" << endl;
+	cout << "rank " << r_id << " updating density" << endl;
 	MPI_Allreduce(&sum, &total_mag, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 	MPI_Barrier(MPI_COMM_WORLD);
 	total_mag = sqrt(total_mag);
@@ -172,7 +168,7 @@ cout << "rank " << r_id << " updating density" << endl;
 	// take a step backward, penalize, and cap
 	for (int d = 0; d < n_design; ++d)
 	{
-		design_density[d] -= t_step_size * design_grad[d]/total_mag;
+		design_density[d] -= t_step_size * design_grad[d] / total_mag;
 
 		if (design_density[d] < MIN_DEN)
 		{
@@ -186,7 +182,6 @@ cout << "rank " << r_id << " updating density" << endl;
 	update_d_mat();
 }
 
-
 vector<int> sx;
 vector<int> sy;
 vector<double> s_amp;
@@ -198,41 +193,43 @@ vector<int> mty;
 vector<double> mEps;
 vector<double> mSig;
 
-
 vector<int> d_x;
 vector<int> d_y;
 
 vector<int> tx;
 vector<int> ty;
 
-void read_in_file(string f_name){
-	
-	if(r_id == 0){
+void read_in_file(string f_name)
+{
+
+	if (r_id == 0)
+	{
 		ifstream file(f_name);
 		string line;
 
 		getline(file, line);
 
-		//Read Field Size
+		// Read Field Size
 		stringstream lineStream;
 		lineStream.str(line);
 
 		lineStream >> NUMROWS;
 		lineStream >> NUMCOLS;
 		cout << "field size read: " << NUMROWS << " x " << NUMCOLS << endl;
-		//Read Time Steps, Top op steps
+		// Read Time Steps, Top op steps
 		getline(file, line);
 		lineStream.clear();
 		lineStream.str(line);
 		lineStream >> fdtd_steps;
 		lineStream >> topop_steps;
 		cout << "step counts read: " << fdtd_steps << " fdtd, " << topop_steps << " topop" << endl;
-		//Read in Source Points
+		// Read in Source Points
 		getline(file, line);
 		lineStream.clear();
 		lineStream.str(line);
 		lineStream >> n_source;
-		for(int i = 0; i < n_source; ++i){
+		for (int i = 0; i < n_source; ++i)
+		{
 			getline(file, line);
 			lineStream.clear();
 			lineStream.str(line);
@@ -248,18 +245,19 @@ void read_in_file(string f_name){
 			sy.push_back(y);
 			s_amp.push_back(amp);
 			s_type.push_back(type);
-			s_off.push_back(off);	
+			s_off.push_back(off);
 		}
 
 		cout << "source info read " << n_source << " sources" << endl;
-		//Read in Material Properties
+		// Read in Material Properties
 		getline(file, line);
 		lineStream.clear();
 		lineStream.str(line);
 		int mat_rects;
 		lineStream >> mat_rects;
 
-		for(int i = 0; i < mat_rects; ++i){
+		for (int i = 0; i < mat_rects; ++i)
+		{
 			getline(file, line);
 			lineStream.clear();
 			lineStream.str(line);
@@ -272,8 +270,10 @@ void read_in_file(string f_name){
 			lineStream >> W;
 			lineStream >> eps;
 			lineStream >> sig;
-			for(int x = 0; x < L; ++x){
-				for(int y = 0; y < W; ++y){
+			for (int x = 0; x < L; ++x)
+			{
+				for (int y = 0; y < W; ++y)
+				{
 					mtx.push_back(TL_X + x);
 					mty.push_back(TL_Y + y);
 					mEps.push_back(eps);
@@ -282,15 +282,16 @@ void read_in_file(string f_name){
 			}
 		}
 		n_mats = mtx.size();
-		cout << "material info read " << n_mats << " mats"  << endl;
-		//Read in targets
+		cout << "material info read " << n_mats << " mats" << endl;
+		// Read in targets
 		getline(file, line);
 		lineStream.clear();
 		lineStream.str(line);
 		int t_rects;
 		lineStream >> t_rects;
 
-		for(int i = 0; i < t_rects; ++i){
+		for (int i = 0; i < t_rects; ++i)
+		{
 			getline(file, line);
 			lineStream.clear();
 			lineStream.str(line);
@@ -299,8 +300,10 @@ void read_in_file(string f_name){
 			lineStream >> TLY;
 			lineStream >> L;
 			lineStream >> W;
-			for(int x = 0; x < L; ++x){
-				for(int y = 0; y < W; ++y){
+			for (int x = 0; x < L; ++x)
+			{
+				for (int y = 0; y < W; ++y)
+				{
 					tx.push_back(TLX + x);
 					ty.push_back(TLY + y);
 				}
@@ -308,9 +311,8 @@ void read_in_file(string f_name){
 		}
 		n_targets = tx.size();
 
-
-		cout << "target info read " << n_targets <<" targets" << endl;
-		//Read in design parameter information
+		cout << "target info read " << n_targets << " targets" << endl;
+		// Read in design parameter information
 		getline(file, line);
 		lineStream.clear();
 		lineStream.str(line);
@@ -318,7 +320,8 @@ void read_in_file(string f_name){
 		lineStream >> dp_rects;
 		lineStream >> d_eps;
 		lineStream >> d_sig;
-		for(int i  = 0; i < dp_rects; ++i){
+		for (int i = 0; i < dp_rects; ++i)
+		{
 			getline(file, line);
 			lineStream.clear();
 			lineStream.str(line);
@@ -327,8 +330,10 @@ void read_in_file(string f_name){
 			lineStream >> TLY;
 			lineStream >> L;
 			lineStream >> W;
-			for(int x = 0; x < L; ++x){
-				for(int y = 0; y < W; ++y){
+			for (int x = 0; x < L; ++x)
+			{
+				for (int y = 0; y < W; ++y)
+				{
 					d_x.push_back(x + TLX);
 					d_y.push_back(y + TLY);
 				}
@@ -337,10 +342,11 @@ void read_in_file(string f_name){
 
 		n_design = d_x.size();
 		cout << "design params done " << n_design << " params" << endl;
-		//allocate necessary pointers
+		// allocate necessary pointers
 	}
-	int * counts = new int[8];
-	if(r_id == 0){
+	int *counts = new int[8];
+	if (r_id == 0)
+	{
 		counts[0] = NUMROWS;
 		counts[1] = NUMCOLS;
 		counts[2] = n_source;
@@ -350,7 +356,7 @@ void read_in_file(string f_name){
 		counts[6] = fdtd_steps;
 		counts[7] = topop_steps;
 	}
-	
+
 	MPI_Barrier(MPI_COMM_WORLD);
 
 	MPI_Bcast(counts, 8, MPI_INT, 0, MPI_COMM_WORLD);
@@ -365,7 +371,6 @@ void read_in_file(string f_name){
 	fdtd_steps = counts[6];
 	topop_steps = counts[7];
 
-
 	dx = (double *)malloc(NUMROWS * NUMCOLS * sizeof(double));
 	ex = (double *)malloc(NUMROWS * NUMCOLS * sizeof(double));
 	hy = (double *)malloc(NUMROWS * NUMCOLS * sizeof(double));
@@ -379,21 +384,25 @@ void read_in_file(string f_name){
 	dxT_abc = (double *)malloc(NUMCOLS * 6 * sizeof(double));
 	dxB_abc = (double *)malloc(NUMCOLS * 6 * sizeof(double));
 
-	d_perr_id = (n_design - 1)/num_procs + 1;
-	lcl_grad = (double *)malloc(d_perr_id*sizeof(double));	
+	d_perr_id = (n_design - 1) / num_procs + 1;
+	lcl_grad = (double *)malloc(d_perr_id * sizeof(double));
 	design_grad = (double *)malloc(num_procs * d_perr_id * sizeof(double));
-	design_density = (double *) malloc(n_design * sizeof(double));
+	design_density = (double *)malloc(n_design * sizeof(double));
 
-	for(int i = 0; i < n_design; ++i){
+	for (int i = 0; i < n_design; ++i)
+	{
 		design_density[i] = 0.5;
 	}
-	for(int i = 0; i < num_procs * d_perr_id; ++i){
+	for (int i = 0; i < num_procs * d_perr_id; ++i)
+	{
 		design_grad[i] = 0.0;
 	}
-	for(int i = 0; i < d_perr_id; ++i){
+	for (int i = 0; i < d_perr_id; ++i)
+	{
 		lcl_grad[i] = 0.0;
 	}
-	if(r_id == 0){
+	if (r_id == 0)
+	{
 		target_x = tx.data();
 		target_y = ty.data();
 
@@ -411,8 +420,9 @@ void read_in_file(string f_name){
 		mat_eps = mEps.data();
 		mat_sig = mSig.data();
 	}
-	else{
-		//malloc pointers that are not on r_id 0	
+	else
+	{
+		// malloc pointers that are not on r_id 0
 		target_x = (int *)malloc(n_targets * sizeof(int));
 		target_y = (int *)malloc(n_targets * sizeof(int));
 
@@ -422,15 +432,13 @@ void read_in_file(string f_name){
 		source_x = (int *)malloc(n_source * sizeof(int));
 		source_y = (int *)malloc(n_source * sizeof(int));
 		source_amp = (double *)malloc(n_source * sizeof(double));
-		source_offset = (double *) malloc(n_source * sizeof(double));
-		source_type = (int *) malloc(n_source * sizeof(int));
+		source_offset = (double *)malloc(n_source * sizeof(double));
+		source_type = (int *)malloc(n_source * sizeof(int));
 
-		mat_x = (int *) malloc(n_mats * sizeof(int));
-		mat_y = (int *) malloc(n_mats * sizeof(int));
-		mat_eps = (double *) malloc(n_mats * sizeof(double));
-		mat_sig = (double *) malloc(n_mats * sizeof(double));
-
-
+		mat_x = (int *)malloc(n_mats * sizeof(int));
+		mat_y = (int *)malloc(n_mats * sizeof(int));
+		mat_eps = (double *)malloc(n_mats * sizeof(double));
+		mat_sig = (double *)malloc(n_mats * sizeof(double));
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
@@ -453,7 +461,8 @@ void read_in_file(string f_name){
 
 	MPI_Barrier(MPI_COMM_WORLD);
 
-	if(r_id == 0){
+	if (r_id == 0)
+	{
 		cout << "storing done" << endl;
 	}
 }
@@ -461,24 +470,23 @@ void read_in_file(string f_name){
 void do_top_op()
 {
 
-
 	init_mat_properties();
 
 	update_d_mat();
 	damage_max = simulate();
 	for (int i = 0; i < topop_steps; ++i)
-	{	
-		if(r_id == 0){
+	{
+		if (r_id == 0)
+		{
 			cout << "doing " << i << " / " << topop_steps << endl;
 		}
 		update_density();
 	}
 }
-void do_top_op_full_out(){
-
+void do_top_op_full_out()
+{
 
 	ofstream cond_file("cond.out");
-
 
 	cout << "starting topop: " << topop_steps << " total steps " << endl;
 	init_mat_properties();
@@ -486,12 +494,11 @@ void do_top_op_full_out(){
 
 	update_d_mat();
 
-
-
-	for(int i = 0; i < NUMROWS; ++i){
-		for(int j = 0; j < NUMCOLS; ++j){
+	for (int i = 0; i < NUMROWS; ++i)
+	{
+		for (int j = 0; j < NUMCOLS; ++j)
+		{
 			cond_file << sigma(i, j) << " ";
-
 		}
 
 		cond_file << endl;
@@ -504,59 +511,63 @@ void do_top_op_full_out(){
 		cout << "step " << i << "/" << topop_steps << " finished" << endl;
 		update_density();
 
-
-		for(int i = 0; i < NUMROWS; ++i){
-			for(int j = 0; j < NUMCOLS; ++ j){
+		for (int i = 0; i < NUMROWS; ++i)
+		{
+			for (int j = 0; j < NUMCOLS; ++j)
+			{
 				cond_file << sigma(i, j) << " ";
-
 			}
 
 			cond_file << endl;
 		}
 		cond_file << endl;
-
-
 	}
 
-
 	cond_file.close();
-
 }
 
-int main(int argc, char *argv [])
-{	
+int main(int argc, char *argv[])
+{
 
 	MPI_Init(&argc, &argv);
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &r_id);
 	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
-	if(r_id == 0){
+	if (r_id == 0)
+	{
 		cout << "Start " << endl;
 	}
 	int output = 0;
 	string file_name;
-	
-	if(argc == 3){
-		output = stoi(argv[1]);	
+
+	if (argc == 3)
+	{
+		output = stoi(argv[1]);
 		file_name = argv[2];
 	}
-	else if(argc == 2){
+	else if (argc == 2)
+	{
 		file_name = argv[1];
 	}
 
 	read_in_file(file_name);
-	if(r_id == 0){
+	if (r_id == 0)
+	{
 		cout << d_eps << " " << d_sig << endl;
 	}
-	if(output == 0){
+	if (output == 0)
+	{
 		do_top_op();
 	}
-	else if(num_procs == 1){
+	else if (num_procs == 1)
+	{
 		do_top_op_full_out();
 	}
-	else{
-		if(r_id == 0){
+	else
+	{
+		if (r_id == 0)
+		{
 			cout << "bad input" << endl;
 		}
 	}
